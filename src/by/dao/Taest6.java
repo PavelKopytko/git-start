8e43dd249f24c4fd980add60d0e6c951610bf436
Pavel Kopytko <kopytkopaul@gmail.com>
2023-01-21 23:51:55 +0300

    some update test5 and test6

--- src/by/dao/Taest6.java
+++ src/by/dao/Taest6.java
@@ -1,4 +1,7 @@
 package by.dao;
 
 public class Taest6 {
+    public static void main(String[] args) {
+
+    }
 }
--- src/by/dao/Test5.java
+++ src/by/dao/Test5.java
@@ -1,5 +1,7 @@
 package by.dao;
 
 public class Test5 {
+    public static void main(String[] args) {
 
+    }
 }
